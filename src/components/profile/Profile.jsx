9f8ca1d17eb447da02ea809da9563deb3bb59eeb
Andry1343 <ua.gavrilets@gmail.com>
2022-11-26 05:32:45 +0200

    Done

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,55 @@
+import css from '../profile/Profile.module.css';
+import PropTypes from 'prop-types';
+
+export const Profile = ({ username, avatar, location, tag, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>
+            <br />
+            {stats.followers}
+          </span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>
+            <br />
+            {stats.views}
+          </span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>
+            <br />
+            {stats.likes}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  user: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.symbol.isRequired,
+      username: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      stats: {
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+      },
+    })
+  ),
+};
